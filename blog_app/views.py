dbc1c671172d038b31db45545a039580302dcc63
weiliangchujin <1915668836@qq.com>
2021-10-09 16:47:35 +0800

    博客详情页开发，加入Markdown语法展示文章内容

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
+import markdown
 
 def index(request):
     post_list = Post.objects.all().order_by('-created_time')
     return render(request, 'blog_app/index.html', context={'post_list': post_list})
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
+                                  ])
+    return render(request, 'blog_app/detail.html', context={'post': post})
